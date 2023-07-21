ed5845c1c04f70c3039da9878279f818c51aaaa8
Ipeng13 <95187786+Ipeng13@users.noreply.github.com>
2023-07-21 18:21:14 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_11.0"
+export DT_LINK="https://github.com/Ipeng13/device_xiaomi_sweet.git -b fox_11.0"
 
-export DEVICE="beryllium"
+export DEVICE="Sweet"
 export OEM="xiaomi"
 
 # Build Target
@@ -22,7 +22,7 @@ DEPS=(
 )
 
 # Extra Command
-export EXTRA_CMD="export OF_MAINTAINER=Sushrut1101"
+export EXTRA_CMD="export OF_MAINTAINER=Ipeng13"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
